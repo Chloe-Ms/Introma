b65ed1020956c8873eb650d309dc2640207d3ddd
BBouleDeSuiff <emaouardi@gmail.com>
2022-12-21 04:40:32 +0100

    Win System (c pa bo)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -31,11 +32,14 @@ public class GameManager : MonoBehaviour
     private MailBoxManager mailBoxManager;
 
     [SerializeField] private Animator FadeImage;
+    [SerializeField] private TMP_Text WinText;
+    [SerializeField] private GameObject WinButton;
 
     void Start()
     {
         //mailBoxManager = GameObject.FindObjectOfType<MailBoxManager>();
         //mailBoxManager.GetDayMail();
+        Win();
     }
 
     // Update is called once per frame
@@ -57,8 +61,32 @@ public class GameManager : MonoBehaviour
         Debug.Log("New day : "+(int)day);
         SceneManager.LoadScene((int)day);
         FadeImage.SetTrigger("FadeIn");
-        //mailBoxManager = GameObject.FindObjectOfType<MailBoxManager>();
-        //mailBoxManager.GetDayMail();
+    }
+    public void Win()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        FadeImage.gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.white;
+        FadeImage.SetTrigger("FadeOut");
+        StartCoroutine(WinCinematic());
+    }
+    IEnumerator WinCinematic()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("EndScene");
+        //FadeImage.gameObject.GetComponent<UnityEngine.UI.Image>().enabled = false;
+        //FadeImage.gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.black;
+        WinText.gameObject.SetActive(true);
+        yield return StartCoroutine(ScrollText.Instance.ScrollSentence("You're free.", WinText));
+        yield return new WaitForSeconds(1f);
+        WinButton.SetActive(true);
+    }
+    public void RemoveWinScreen()
+    {
+        WinButton.SetActive(false);
+        FadeImage.gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.clear;
+        WinText.text = "";
+        WinText.gameObject.SetActive(false);
     }
     private int GetNewDay(int currentDay, int numberOfDays)
     {
